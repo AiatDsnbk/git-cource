a3305b7c8fa3d32feb35a1508cd221f115321aa2
AiatDsnbk <tatosh2000ayat@gmail.com>
2021-07-13 10:09:01 +0600

    crontabwork

--- python1.py
+++ python1.py
@@ -1,1 +1,13 @@
-print("Let's go to Korea.")
+import urllib.request
+from bs4 import BeautifulSoup
+import requests
+import os
+def main():
+    page = urllib.request.urlopen("https://www.cbr.ru/")
+    soup = BeautifulSoup(page, features="lxml")
+    h = soup.find('div', {'class': 'col-md-2 col-xs-9 _right mono-num'}).text
+    return send_message(h)
+def send_message(message):
+    title = 'Dollar: '
+    os.system('notify-send "{}" "{}"'.format(title, message))
+main()
--- riafanru_boevoi.py
+++ riafanru_boevoi.py
@@ -1,12 +1,16 @@
+#https://riafan.ru/news
 import ibm_db_dbi as db
 import requests
 import re
 from bs4 import BeautifulSoup
-import requests
+import urllib.request
 import urllib
+import psycopg2
+from psycopg2 import Error
+from datetime import date, timedelta
+import datetime
 
 def telegram_bot_sendtext(bot_message):
-
     bot_token = '1700296384:AAGoaq3o2WD7BbUthmU9dEYQD5IMEKBIgxU'
     bot_chatID = '771779345'
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
@@ -27,76 +31,54 @@ try:
     page.status_code
     soup = BeautifulSoup(page.text, 'html.parser')
     url_statyi = soup.find_all(class_ = 'cat-news-item')
-    url_public = soup.find_all(class_ = 'cat-item__title ')
     urls = []
+    vi = []
+    
     for i in range(len(url_statyi)):
-        url1 = ""
-        url1 = soup.find_all(class_='cat-item__title ')[i].findAll('a')[0]["href"]
+        url2 = soup.find_all(class_ = 'cat-news-item')[i]
+        url1 = url2.findAll('a')[0]["href"]
+        
+        v = url2.find(class_ = 'cat-item__views tile-list-news__views')
+        view = v.get_text()
+        vi.append(view)
+        
         urls.append(url1)
-    q = 0
+        q = 0
+        
     for i in range(len(urls)):
-        URL = "https://riafan.ru" + urls[i]
-        url = urls[i][1:]
+        URL = urls[i]
+        url = urls[i][0:]
         object_id = converter(url)
+        
+        views = vi[i]
+        
         page = requests.get(URL)
         page.status_code
         soup =  BeautifulSoup(page.text, 'html.parser')
-        text = soup.find(class_ = 'col-md-12 col-lg-8 post__text user-content')
-        text1 = text.get_text()[:-46]
-        dat = soup.find(class_ = 'article__date')
-        vi = soup.find(class_ = 'article__views')
-        views = vi.get_text()
-        da = dat.get_text()
-        daa = da.replace(" ","")
-        dat = ""
-        for t in range(len(daa)):
-            if daa[t] != "\n" :
-                dat = dat + daa[t]
-        clock = dat[-5:]
-        day = dat[0:2]
-        for d in range(len(dat)):
-            if dat[d] ==",":
-                m = d
-                break
+        
+        text1 = soup.find(class_ = 'content-wrap')
+        text = text1.get_text()
+        
+        date = soup.find(class_ = 'post-published').get('datetime')
+        d = re.findall('\d+', date)
+        date_time = d[0] + '-' + d[1] + '-' + d[2] + ' ' + d[3] + ':' + d[4] + ':' + '00.0'
+        
+        time1 = d[3] + ':' + d[4] + ':' + '00'
+        tlist = [time1, '3:00:00']
+        mysum = datetime.timedelta()
 
-        data = ""
-        if  dat[2:m] == "мая":
-            mouth = "05"
-
-        if  dat[2:m] == "июня":
-            mouth = "06"
-
-        if  dat[2:m] == "июля":
-            mouth = "07"
-
-        if  dat[2:m] == "августа":
-            mouth = "08"
-
-        if  dat[2:m] == "сентября":
-            mouth = "09"
-
-        if  dat[2:m] == "октября":
-            mouth = "10"
-
-        if  dat[2:m] == "ноября":
-            mouth = "11"
-
-        if  dat[2:m] == "декабря":
-            mouth = "12"
-
-        if  dat[2:m] == "января":
-            mouth = "01"
-
-        if  dat[2:m] == "февраля":
-            mouth = "02"
-
-        if  dat[2:m] == "марта":
-            mouth = "03"
-
-        if  dat[2:m] == "апреля":
-            mouth = "04"
-        data = "2019-" + mouth + "-" + day + " " + clock + ":00"
-
+        for i in tlist:
+            (h, m, s) = i.split(':')
+            dt = datetime.timedelta(hours = int(h), minutes = int(m), seconds = int(s))
+            mysum += dt
+            date = d[0] + '-' + d[1] + '-' + d[2] + ' ' + str(mysum) + '.0'
+            
+        try:
+            img = soup.find('div', {'class': 'main-image-container'})
+            url_attachment = img.find('img').get('data-src')
+        except:
+            url_attachment = None
+        
         connection_text = """DATABASE=PRODDB;HOSTNAME=192.168.252.11;PORT=50000;
         PROTOCOL=TCPIP;UID=db2inst1;PWD=Qjuehnghj1;"""
         sql_1_test = "SELECT url_channel FROM TL_MEDIA_DATA_NEWS_TEST"
@@ -107,44 +89,29 @@ try:
         con.commit()
         cursor.close()
         con.close()
+        
         t0 = []
         for y in range(len(t)):
             t0.append(t[y][0])
+            
         if not URL in t0:
             connection_text = """DATABASE=PRODDB;HOSTNAME=192.168.252.11;
             PORT=50000;PROTOCOL=TCPIP;UID=db2inst1;PWD=Qjuehnghj1;"""
             con = db.connect(connection_text, "", "")
             cursor = con.cursor()
-            sql_1_test = """insert into TL_MEDIA_DATA_NEWS_TEST (object_id, published_date, channel_id, likes, 
-            comments, views, reposts, 
-            caption, text, url_attachment, url_channel, source_id) 
-            values (?,?,?,?,?,?,?,?,?,?,?,?)"""
-            cursor.execute(sql_1_test,  (object_id, data, 1, None, None, views, None, None, text1, None, URL, 922))
+            insert_query = """insert into TL_MEDIA_DATA_NEWS_TEST (object_id, published_date, channel_id, 
+            likes, comments, views, reposts, caption, text, url_attachment, url_channel, source_id) 
+            values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"""
+            cursor.execute(insert_query, (object_id, date, None, None, None, views, None, None, text, url_attachment, URL, 700))
             con.commit()
             cursor.close()
             con.close()
             q = q + 1
-            
-    # #Select data from table here
-    # connection_text = """DATABASE=PRODDB;HOSTNAME=192.168.252.11;PORT=50000;
-    #     PROTOCOL=TCPIP;UID=db2inst1;PWD=Qjuehnghj1;"""
-    # con = db.connect(connection_text, "", "")
-    # cursor = con.cursor()
-    # cursor.execute("""
-    # SELECT url_channel
-    # FROM TL_MEDIA_DATA_NEWS_TEST
-    # ORDER BY published_date""")
-    # result = cursor.fetchall()
-    # con.commit()
-    # cursor.close()
-    # con.close()
-    # for x in result:
-    #     print(x)
-        
-    send_text = "Парсинг сайта riafan.ru  успешно прошло, добавлено " + str(q) + " публикации. Благодарю."
-    telegram_bot_sendtext(send_text)
-    print(send_text)
-except :
-    send_text = "Ошибка при парсинге(riafan.ru)"
-    telegram_bot_sendtext(send_text)
-    print(send_text, error)
+    #"Парсинг riafan.ru прошел успешно.(time.kz)" 
+    send_text = "Парсинг сайта riafan.ru успешно прошло, добавлено " + str(q) + " публикации. Благодарю."
+    telegram_bot_sendtext(send_text)   #Сообщение отправлено в телеграмм бот
+    #print(send_text)
+except (Exception, psycopg2.Error) as error:
+    send_text = "Ошибка при парсинге сайта riafan.ru " + "\n" + str(error)
+    telegram_bot_sendtext(send_text)   #Сообщение отправлено в телеграмм бот
+    #print(send_text)
